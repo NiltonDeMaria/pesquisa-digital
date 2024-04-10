2786ecd5285c0f63d9181e7f8f931a19bb520218
matoko <matheuslisboabr@gmail.com>
2024-04-10 20:35:49 -0300

    edit class Pergunta para Pesquisa
    edit index.py e perguntas.py , troca de nome na class Pergunta para Pesquisa e add  seus métodos: realizarPesquisa e salvarPesquisas

--- index.py
+++ index.py
@@ -1,37 +1,9 @@
-import csv
-from datetime import datetime
-import perguntas as p
+from perguntas import Pesquisa
 
-codigo = input('Digite o código de identificação da pesquisa: ')
-
-with open(f'respostas{codigo}.csv', 'w', newline='') as csvfile:
-            campos_head = ['idade', 'genero', 'resposta_1', 'resposta_2', 'resposta_3', 'resposta_4', 'data e hora']
-            writer = csv.DictWriter(csvfile, fieldnames=campos_head)
-
-            writer.writeheader()
-            
-            idade = 3
-
-            while idade != 00:
-                     
-                idade = int(input("Digite a idade do usuário: "))
-                
-                if idade == 00:
-                    break
-
-                genero = input("Digite o gênero do usuário: ")
-
-                resposta1 = p.Pergunta(1, "Nos últimos 3 meses você comprou chocolate nas Lojas Americanas?").pergunta()
-                
-                resposta2 = p.Pergunta(2, "Nos últimos 3 meses você comprou bebida nas Lojas Americanas?").pergunta()
-                
-                resposta3 = p.Pergunta(3, "Nos últimos 3 meses você comprou item de papelaria nas Lojas Americanas?").pergunta()
-                
-                resposta4 = p.Pergunta(4 , "Nos últimos 3 meses você comprou livro nas Lojas Americanas?").pergunta()
-
-                data_hora = datetime.now().strftime('%d/%m/%Y %H:%M')
-                
-
-                writer.writerow({'idade': idade, 'genero': genero, 'resposta_1':resposta1, 'resposta_2':resposta2, 'resposta_3': resposta3, 'resposta_4':resposta4, 'data e hora': data_hora})
-
+# talvez acrescentar uma def main() para inicia o codigo 
+# def main():
+codigo_pesquisa = input('Digite o código de identificação da pesquisa: ')
+pesquisa = Pesquisa(codigo_pesquisa)
+pesquisa.realizarPesquisa()
+pesquisa.salvarPesquisas()
         
--- perguntas.py
+++ perguntas.py
@@ -1,14 +1,54 @@
-class Pergunta:
-    def __init__(self, numero, texto):
-        self.numero = numero
-        self.texto = texto
-    
-    def pergunta(self):
-        visual = int(input(f'''
-            Pergunta {self.numero}:
-                {self.texto}
-                1 - Sim
-                2 - Não
-                3 - Não sei responder
-                          '''))
-        return visual
+import csv
+from datetime import datetime
+
+class Pesquisa:
+    def __init__(self, codigo):
+        self.codigo = codigo
+        # perguntas 
+        self.perguntas = [
+            "Nos últimos 3 meses você comprou chocolate nas Lojas Americanas?",
+            "Nos últimos 3 meses você comprou bebida nas Lojas Americanas?",
+            "Nos últimos 3 meses você comprou item de papelaria nas Lojas Americanas?",
+            "Nos últimos 3 meses você comprou livro nas Lojas Americanas?"
+        ]
+        
+        self.respostas = []
+
+    def realizarPesquisa(self):
+        # enquanto for verdade o codigo rodando 
+        while True:
+
+             # falta verificaçao de valores /tratamento de input
+            idade = input("Digite a idade do usuário: ")
+            if idade == '00':
+                break
+            genero = input("Digite o gênero do usuário: ")
+
+            # criaçao de dict respostasUsuario com idade e genero.
+            respostasUsuario = {'idade': idade, 'genero': genero}
+
+            # perginta - loop for para pega perguntas, i - enumerate ira enumera a quantidade de perguntas na lista self.perguntas.
+            for i, pergunta in enumerate(self.perguntas, start=1):
+                resposta = input(f"{pergunta}\n1 - Sim\n2 - Não\n3 - Não sei responder\nResposta: ")
+        
+                # dependedo da quantidade de perguntas teremos a mesma quantidades de respostas.
+                respostasUsuario[f'resposta_{i}'] = resposta 
+
+            respostasUsuario['data_e_hora'] = datetime.now().strftime('%d/%m/%Y %H:%M') # data hora
+            
+            #  acrescentado respostasUsuario na lista self.respostas dentro de atributos Pesquisa fora do loop
+            self.respostas.append(respostasUsuario)
+
+
+    def salvarPesquisas(self):
+        with open(f'respostas{self.codigo}.csv', 'w', newline='', encoding='utf-8') as csvfile:
+
+            # criando cabeçalho com os valores chaves do primeiro elemento da lista self.respostas
+            campos_head = [x for x in self.respostas[0]]
+
+            writer = csv.DictWriter(csvfile, fieldnames=campos_head)
+            writer.writeheader()
+            for resposta in self.respostas:
+                writer.writerow(resposta)
+
+
