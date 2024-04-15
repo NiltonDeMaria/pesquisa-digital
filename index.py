296406db3c23acd1a6399eaba9718398f7f2aa33
Leonardo Xavier <101280914+leeoxavi@users.noreply.github.com>
2024-04-15 17:35:29 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,17 +1,22 @@
-from perguntas import *
+from datetime import datetime
+import csv
+from perguntas import Pergunta, Pesquisa  # Importando classes relevantes do arquivo perguntas.py
 
-#Definindo as perguntas como uma lista fora da classe pesquisa e utilizando a classe Pergunta() para formatação do visual e validção das respostas
-perguntas =  [
-            Pergunta(1, "Você possui atualmente um seguro de vida?"),
-            Pergunta(2, "Você sabe o que é um seguro de vida?"),
-            Pergunta(3, "Você acredita que um seguro de vida é importante para proteger sua família financeiramente em caso de sua morte?"),
-            Pergunta(4, "Você já considerou adquirir um seguro de vida para garantir o futuro de seus entes queridos?"),
-            Pergunta(5, "Você sabe como funcionam os diferentes tipos de seguros de vida, como seguro de vida inteira e seguro de vida temporário?"),
-            Pergunta(6, "Você sabe como funcionam os diferentes tipos de seguros de vida, como seguro de vida inteira e seguro de vida temporário?")
-        ]
+# Definindo as perguntas como uma lista fora da classe pesquisa e utilizando a classe Pergunta() para formatação do visual e validação das respostas
+perguntas = [
+    Pergunta(1, "Você possui atualmente um seguro de vida?"),  # Criando instâncias da classe Pergunta com número e texto
+    Pergunta(2, "Você sabe o que é um seguro de vida?"),
+    Pergunta(3, "Você acredita que um seguro de vida é importante para proteger sua família financeiramente em caso de sua morte?"),
+    Pergunta(4, "Você já considerou adquirir um seguro de vida para garantir o futuro de seus entes queridos?"),
+    Pergunta(5, "Você sabe como funcionam os diferentes tipos de seguros de vida, como seguro de vida inteira e seguro de vida temporário?"),
+    Pergunta(6, "Você sabe como funcionam os diferentes tipos de seguros de vida, como seguro de vida inteira e seguro de vida temporário?")
+]
 
+codigo_pesquisa = input('Digite o código de identificação da pesquisa: ')  # Solicita ao usuário o código de identificação da pesquisa
 
-codigo_pesquisa = input('Digite o código de identificação da pesquisa: ')
-pesquisa = Pesquisa(codigo_pesquisa , perguntas) #Iniciando a pesquisa com os parametros de codigo e das perguntas da lista
+# Cria uma instância da classe Pesquisa com o código fornecido e a lista de perguntas
+pesquisa = Pesquisa(codigo_pesquisa, perguntas)
+
+# Realiza a pesquisa e salva as respostas
 pesquisa.realizarPesquisa()
 pesquisa.salvarPesquisas()
