ebeeeb0470b5c08826ceb66b7e64493718b34edc
Raphaela Vargas <raphaela.vargas@hotmail.com>
2024-04-18 09:45:32 -0300

    Modificando a classe pergunta para retornar o valor inserido pelo usuario, e adicionando opcoes de genero.

--- index.py
+++ index.py
@@ -1,34 +1,25 @@
-from datetime import datetime
-import csv
-from perguntas import Pergunta, Pesquisa  # Importando classes relevantes do arquivo perguntas.py
+from perguntas import Pergunta, Pesquisa  
 
-# Definindo as perguntas como uma lista fora da classe pesquisa e utilizando a classe Pergunta() para formatação do visual e validação das respostas
-perguntas = [
-    Pergunta(1, "Você possui atualmente um seguro de vida?"),  # Criando instâncias da classe Pergunta com número e texto
-    Pergunta(2, "Você sabe o que é um seguro de vida?"),
-    Pergunta(3, "Você acredita que um seguro de vida é importante para proteger sua família financeiramente em caso de sua morte?"),
-    Pergunta(4, "Você já considerou adquirir um seguro de vida para garantir o futuro de seus entes queridos?"),
-    Pergunta(5, "Você sabe como funcionam os diferentes tipos de seguros de vida, como seguro de vida inteira e seguro de vida temporário?"),
-    Pergunta(6, "Você sabe como funcionam os diferentes tipos de seguros de vida, como seguro de vida inteira e seguro de vida temporário?")
-]
-
-
-
-def obterCodigoPesquisa():
-    while True:
-        codigoPesquisa = input('Digite o código de identificação da pesquisa: ')
-        if codigoPesquisa.isdigit(): # Aceita apenas valores numericos 
-            return codigoPesquisa
-        else:
-            print("Por favor, digite um código de pesquisa válido.")
-
+# Cria uma instância da classe Pesquisa utilizando como argumento a lista de perguntas. 
 def main():
-    codigo_pesquisa = obterCodigoPesquisa ()
-    # Cria uma instância da classe Pesquisa com o código fornecido e a lista de perguntas
-    pesquisa = Pesquisa(codigo_pesquisa, perguntas)
+
+    # Definindo a lista de perguntas e utilizando a Classe Pergunta().
+    # Criando instâncias da classe Pergunta com número e texto
+    perguntas = [
+        Pergunta(1, "Você sabe o que é um seguro de vida?"), 
+        Pergunta(2, "Você possui atualmente um seguro de vida?"),
+        Pergunta(3, "Você acredita que um seguro de vida é importante para proteger sua família financeiramente em caso de sua morte?"),
+        Pergunta(4, "Você confia na seguradora para cumprir suas obrigações em caso necessário?"),
+        Pergunta(5, "Você já teve alguma experiência pessoal ou familiar em que um seguro de vida foi acionado?"),
+        Pergunta(6, "Você considera o custo do seguro de vida um fator importante ao decidir se deve ou não adquiri-lo?")
+    ]
+    pesquisa = Pesquisa(perguntas)
+    pesquisa.verificaCodigo('Digite o código de identificação da pesquisa: ')  # Solicita e verifica o código fornecido.
+
     # Realiza a pesquisa e salva as respostas
-    pesquisa.realizarPesquisa()
+    pesquisa.realizarPesquisa('Digite a idade do usuário: ', 'Digite o gênero do usuário (F/f para feminino, M/m para masculino ou O/o para outros): ')
     pesquisa.salvarPesquisas()
+    
     
 if __name__ == "__main__":
     main()
--- perguntas.py
+++ perguntas.py
@@ -1,20 +1,17 @@
-# Importa o módulo datetime para manipulação de data e hora
 from datetime import datetime  
-
-# Importa o módulo csv para trabalhar com arquivos CSV
 import csv  
 
-# Definição da classe Pergunta
+# Classe Pergunta: Define o visual e opções de resposta para cada pergunta na pesquisa.
+# Solicita uma resposta ao usuário e a converte para 'Sim', 'Não' ou 'Não sei responder'. Trata os possíveis erros de entrada
 class Pergunta:
     def __init__(self, numero, texto):
-        self.numero = numero  # Atributo para armazenar o número da pergunta
-        self.texto = texto    # Atributo para armazenar o texto da pergunta
+        self.numero = numero          
+        self.texto = texto         
+        self.opcoes  = [1, 2, 3]    
     
-    # Método para apresentar a pergunta ao usuário e obter a resposta
     def pergunta(self):
-        while True: 
+        while True:
             try:
-                # Apresenta a pergunta ao usuário e solicita a resposta
                 resposta = int(input(f''' 
 
 Pergunta {self.numero}:
@@ -23,90 +20,83 @@ Pergunta {self.numero}:
 2 - Não
 3 - Não sei responder
 =>  '''))
-                # Verifica se a resposta está dentro das opções válidas (1, 2 ou 3)
-                if resposta in [1, 2, 3]:
-                    return resposta
+                if resposta in self.opcoes:  
+                   return 'Sim' if resposta == 1 else ('Não' if resposta == 2 else 'Não sei responder')
                 else:
                     print("Resposta inválida! Digite o número que corresponde à sua resposta!")     
             except ValueError:
-                # Trata a exceção se o usuário não inserir um número válido
-                print("Resposta inválida! Digite o número que corresponde à sua resposta!")
+                print("Resposta inválida! Digite o número que corresponde à sua resposta!") 
 
-# Definição da classe Pesquisa
+
+# Classe Pesquisa: Realiza a pesquisa com base em uma lista de perguntas.
 class Pesquisa:
-    def __init__(self, codigo, perguntas):
-        self.codigo = codigo        # Atributo para armazenar o código da pesquisa
-        self.perguntas = perguntas  # Atributo para armazenar a lista de perguntas
-        self.respostas = []         # Atributo para armazenar as respostas dos usuários
+    def __init__(self, perguntas):
+        self.perguntas = perguntas                 
+        self.respostas = []                       
+    
+  # Verifica e armazena o código da pesquisa, garantindo que seja preenchido.  
+    def verificaCodigo(self, msg):               
+        while True:
+            self.codigo = input(msg)              
+            if self.codigo.strip():
+               break                 
+            else:
+                print("Digite o código de pesquisa! ")   
 
-    # Método para realizar a pesquisa
-    def realizarPesquisa(self):
+
+  # Como regra de negocio a pesquisa só poderá ser feita com pessoas acima de 18 anos Sendo digitado idade = 00 a pesquisa é interrompida.
+  # Coleta e verifica a idade do usuário. Trata possiveis erros de entrada.
+    def realizarPesquisa(self, idadePergun, generoPergun):        
         while True:
             idade = None
             genero = None
-
             try:
-                # Solicita a idade do usuário
-                idade = int(input("Digite a idade do usuário (ou digite 00 para sair): "))
-
-                # Verifica se o usuário digitou 0 para sair da pesquisa
-                if idade == 0:
+                idade = int(input(idadePergun))   
+                if idade == 00:             
                     print("Saindo da pesquisa...")
-                    return
-                
-                # Verifica se a idade é menor que 18 anos
-                elif idade < 18:
+                    return 
+                elif idade < 18:            
                     print("A idade mínima para responder a pesquisa é 18 anos!")
                     continue
             except ValueError:
-                
-                # Trata exceção se o usuário digitar algo que não é um número
-                print('Digite uma resposta válida!')
+                print('Digite uma resposta válida!')        
                 continue
 
+  #Coleta e verifica a entrada de gênero.
             while True:
-                # Solicita o gênero do usuário
-                genero = input("Digite o gênero do usuário (F/f para feminino, M/m para masculino): ").strip().upper()
-                # Verifica se o gênero é válido (F ou M)
-                if genero in ['F', 'M']:
-                    genero = 'Feminino' if genero == 'F' else 'Masculino'
-                    break
+                opcoesGenero =  ['F', 'M', 'O' ]                      
+                genero = input(generoPergun).strip().upper()
+                if genero in opcoesGenero:
+                    genero = 'Feminino' if genero == 'F' else 'Masculino' if genero == 'M' else 'Outros' 
+                    break   
                 else:
-                    print("Resposta inválida. Por favor, insira apenas F/f para feminino ou M/m para masculino.")
+                    print("Resposta inválida. Por favor, insira apenas F/f para feminino, M/m para masculino ou O/o para outros")
 
-            # Cria um dicionário com as respostas do usuário
-            respostasUsuario = {'idade': idade, 'genero': genero}
 
-            # Pergunta todas as perguntas e armazena as respostas no dicionário
-            for perg in self.perguntas:
-                resposta = perg.pergunta()
+  # Para cada pergunta na lista de perguntas o método pergunta(Classe Pergunta()) é acionado e armazena a resposta em "resposta"
+  # Cria as colunas do arquivo csv. As respostas são adicionadas ao dicionário (respostaUsuario) juntamente com a data e hora.
+            respostasUsuario = {'idade': idade, 'genero': genero}   
+            for perg in self.perguntas:      
+                resposta = perg.pergunta() 
                 respostasUsuario[f'resposta_{perg.numero}'] = resposta
 
-            # Adiciona a data e hora atual ao dicionário de respostas
-            respostasUsuario['data_e_hora'] = datetime.now().strftime('%d/%m/%Y %H:%M')
+            respostasUsuario['data_e_hora'] = datetime.now().strftime('%d/%m/%Y %H:%M') 
             
-            # Adiciona o dicionário de respostas à lista de respostas da pesquisa
-            self.respostas.append(respostasUsuario)
+            self.respostas.append(respostasUsuario) 
 
-    # Método para salvar as respostas em um arquivo CSV
-    def salvarPesquisas(self):
+
+  # Verifica se há respostas para salvar, caso não haja, imprime uma mensagem informando ao usuário
+  # Obtém os nomes das colunas a partir da chave do primeiro dicionário resposta.
+  # Armazena as respostas do usuário no arquivo csv.
+    def salvarPesquisas(self):          
         with open(f'respostas{self.codigo}.csv', 'w', newline='', encoding='utf-8') as csvfile:
 
-            # Verifica se há respostas para salvar
-            if self.respostas:
+            if self.respostas: 
+                campos_head = list(self.respostas[0].keys())   
+                writer = csv.DictWriter(csvfile, fieldnames=campos_head)    # Cria um escritor de CSV com os campos (colunas) definidos
+                writer.writeheader()                                    # Escreve o cabeçalho no arquivo CSV
 
-                # Obtém os nomes das colunas (cabeçalho) a partir das chaves do primeiro dicionário de respostas
-                campos_head = list(self.respostas[0].keys())
-
-                # Cria um escritor de CSV com os campos (colunas) definidos
-                writer = csv.DictWriter(csvfile, fieldnames=campos_head)
-
-                # Escreve o cabeçalho no arquivo CSV
-                writer.writeheader()
-
-                # Escreve cada linha (resposta) no arquivo CSV
-                for resposta in self.respostas:
+                for resposta in self.respostas:   
                     writer.writerow(resposta)
             else:
-                # Caso não haja respostas, imprime uma mensagem informando ao usuário
-                print("Não há respostas para salvar.")
+                print("Não há respostas para salvar.")    
